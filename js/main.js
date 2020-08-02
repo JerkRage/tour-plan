1d0a574e6c528d0e0b6b94b6b1b85d34e55da4f6
JerkRage <jon56hops@gmail.com>
2020-08-02 22:33:45 +0700

    create: added modal form

--- js/main.js
+++ js/main.js
@@ -24,3 +24,24 @@ menuButton.addEventListener('click', function () {
   console.log('Клик по меню');
   document.querySelector(".navbar-buttom").classList.toggle('navbar-buttom--visible');
 });
+
+var modalButton = $('[data-toggle="modal"]');
+var closeModalButton = $('.modal__close');
+var keyboardCloseModal
+modalButton.on('click', openModal);
+closeModalButton.on('click', closeModal);
+
+function openModal() {
+  var modalOverlay = $('.modal__overlay');
+  var modalDialog = $('.modal__dialog');
+  modalOverlay.addClass('modal__overlay--visible');
+  modalDialog.addClass('modal__dialog--visible');
+};
+
+function closeModal(event) {
+  event.preventDefault();
+  var modalOverlay = $('.modal__overlay');
+  var modalDialog = $('.modal__dialog');
+  modalOverlay.removeClass('modal__overlay--visible');
+  modalDialog.removeClass('modal__dialog--visible');
+};
--- js/map.js
+++ js/map.js
@@ -1,34 +1,36 @@
-ymaps.ready(init);
+$(document).ready(function () {
+  ymaps.ready(init);
 
-function init() {
-  var myMap = new ymaps.Map("map-hotel", {
-      center: [7.890703, 98.294772],
-      zoom: 15,
-    }, {
-      searchControlProvider: 'yandex#search'
-    }),
-    HintLayout = ymaps.templateLayoutFactory.createClass("<div>" +
-      "<b>{{ properties.object }}</b><br>", {
-        getShape: function () {
-          var el = this.getElement(),
-            result = null;
-          if (el) {
-            var firstChild = el.firstChild;
-            result = new ymaps.shape.Rectangle(
-              new ymaps.geometry.pixel.Rectangle([
-                [0, 0],
-                [firstChild.offsetWidth, firstChild.offsetHeight]
-              ])
-            );
+  function init() {
+    var myMap = new ymaps.Map("map-hotel", {
+        center: [7.890703, 98.294772],
+        zoom: 15,
+      }, {
+        searchControlProvider: 'yandex#search'
+      }),
+      HintLayout = ymaps.templateLayoutFactory.createClass("<div>" +
+        "<b>{{ properties.object }}</b><br>", {
+          getShape: function () {
+            var el = this.getElement(),
+              result = null;
+            if (el) {
+              var firstChild = el.firstChild;
+              result = new ymaps.shape.Rectangle(
+                new ymaps.geometry.pixel.Rectangle([
+                  [0, 0],
+                  [firstChild.offsetWidth, firstChild.offsetHeight]
+                ])
+              );
+            }
+            return result;
           }
-          return result;
         }
-      }
-    );
-  var myPlacemark = new ymaps.Placemark([7.890703, 98.294772], {
-    object: "GRAND HILTON HOTEL"
-  }, {
-    hintLayout: HintLayout
-  });
-  myMap.geoObjects.add(myPlacemark);
-}
+      );
+    var myPlacemark = new ymaps.Placemark([7.890703, 98.294772], {
+      object: "GRAND HILTON HOTEL"
+    }, {
+      hintLayout: HintLayout
+    });
+    myMap.geoObjects.add(myPlacemark);
+  };
+});
